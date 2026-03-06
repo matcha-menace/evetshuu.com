14b91fdf8ddf479a58eac7fa3453a630fdc3798b
Steve Hu <yh3133@nyu.edu>
2026-03-06 14:52:09 -0500

    words update

--- js/index.js
+++ js/index.js
@@ -120,6 +120,7 @@ function PageSetup(showSocial, currentPageNavLink, isCat) {
     $("#header-social").addClass(showSocial ? "" : "d-md-none");
     $("#header-cat").addClass(isCat ? "" : "d-none");
     $("#header-stuff").addClass(isCat ? "d-none" : "");
+    $("#header-hamburger").addClass(isCat ? "" : "d-none");
     LoadSvgs();
     // stuff logo animation
     $("#header-stuff")
